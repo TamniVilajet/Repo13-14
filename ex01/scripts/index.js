78156fff49fe892abab5736e5fc32c48ac4b37d6
TamniVilajet <ermin.kirlic@gmail.com>
2022-04-11 01:20:40 +0200

    Project 2

---
+++ ex01/scripts/index.js
@@ -0,0 +1,31 @@
+const modalTriggers = document.querySelectorAll('.modal-trigger');
+
+const modalClose = document.querySelectorAll('.modal-close');
+
+
+// Set click event handlers on each of the trigger links.
+
+Array.from(modalTriggers).forEach(trigger => {
+  trigger.addEventListener('click', function(e) {
+    e.preventDefault();
+    // Grab the modal name that should be shown
+    const modalName = trigger.dataset.modal;
+    // Open that specific modal
+    const modal = document.querySelector('.' + modalName);
+    modal.classList.add('modal--is-open');
+  });
+});
+
+// Set click event handlers on the different close modal triggers.
+
+Array.from(modalClose).forEach(closeSymbol => {
+
+  closeSymbol.addEventListener('click', function() {
+
+    // Find the closest modal and close it.
+    
+    this.closest('.modal').classList.remove('modal--is-open');
+
+  });
+
+});
